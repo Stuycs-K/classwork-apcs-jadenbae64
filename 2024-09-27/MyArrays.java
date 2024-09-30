cdff411de8b804ddb5f852d501723d8f74f199e8
Jaden Bae <jbae50@stuy.edu>
2024-09-30 08:59:11 -0400

    added arrayToString

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -27,12 +27,27 @@ public class MyArrays{
         return newArray;
     }
     
-    
+    public static int[] concatArray(int[]ary1,int[]ary2){
+        int[] newArray = new int[ary1.length + ary2.length];
+        
+        for (int i = 0; i < ary1.length; i++){
+            newArray[i] = ary1[i];
+        }
+
+        for (int i = 0; i < ary2.length; i++){
+            newArray[i + ary1.length] = ary2[i];
+        }
+
+        return newArray;
+
+
+    }
     public static void main(String[]args){
         int myArray[] = {3, 4, 3, 2};
+        int myArray2[] = {2, 3 , 4};
         System.out.println(arrayToString(myArray));
         System.out.println(!(myArray == returnCopy(myArray))); //returns true when they point at different array
-                
+        System.out.println(arrayToString(concatArray(myArray, myArray2)));
 
     }
 
