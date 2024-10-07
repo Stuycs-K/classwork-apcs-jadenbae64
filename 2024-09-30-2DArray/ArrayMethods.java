55bda103edbc882934c4d3077f11fdc88ca5768e
Jaden Bae <jbae50@stuy.edu>
2024-10-07 07:24:47 -0400

    fixed file name, arrToString method name, index bug

---
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -0,0 +1,86 @@
+//anjali bechu
+//abechu60@stuy.edu
+
+//jaden bae
+//jabae50@stuy.edu
+
+public class ArrayMethods{
+    public static void main(String[] args){
+
+      //returns true if 2D Sum matches the sum  
+      int[][] arr = {{2,3,4},{5,6,7},{8,0,4}};
+      System.out.println(arr2DSum(arr) == 39);
+
+      arr = {{2,3,4},{5,6,7},{8,0,4}};
+      System.out.println(arr2DSum(arr) == 39);
+
+      System.out.println(arrToString(arr));
+      
+
+      // int [] [] array{
+      //{2, 3, 4},
+      //{5, 6, 7},
+      //{8, 0, 4};
+      //
+      //String result = arrToString(array)
+      //System.out.println= (arrToString(result))
+
+
+    }
+
+    public static String arrToString(int[] ary){
+        String combined = "[";
+        for (int i = 0; i < ary.length; i++){
+            if (i == ary.length - 1){
+                combined += ary[i];
+
+            }else{
+                combined += ary[i] + ", ";
+
+            }
+        }
+
+        combined += "]";
+        return combined;
+    }
+
+    public static String arrToString(int[][] ary){
+        String combined = "[";
+        for (int i = 0; i < ary.length; i++){
+            if (i == ary.length - 1){
+                combined += arrToString(ary[i]);
+
+            }else{
+                combined += arrToString(ary[i]) + ", ";
+
+            }
+        }
+
+        combined += "]";
+        return combined;
+    }
+
+    public static int arr2DSum(int[][]nums){
+        int combinedSum = 0;
+        for (int i = 0; i < nums.length; i++){
+            for (int index = 0; index < nums[i].length; index++){
+                combinedSum = combinedSum + nums[i][index];
+            }
+        }
+
+        return combinedSum;
+    }
+    
+    public static int[][] swapRC(int[][]nums){
+        
+        int[][] newArray = new int[nums[0].length][nums.length];
+        for (int i = 0; i < nums.length; i++){
+            for (int index = 0; i < nums[i].length; index++){
+                newArray[index][i] = nums[i][index];
+            }
+        
+        }
+            
+        return newArray;
+    }
+}
