4c7c7c9e6d1d337883756e0443bed173a8490609
Jaden Bae <jbae50@stuy.edu>
2024-10-07 07:40:46 -0400

    added test cases for swapRC and more test cases for other methods in addition

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -11,11 +11,25 @@ public class ArrayMethods{
       int[][] arr = {{2,3,4},{5,6,7},{8,0,4}};
       System.out.println(arr2DSum(arr) == 39);
 
-      arr = {{2,3,4},{5,6,7},{8,0,4}};
-      System.out.println(arr2DSum(arr) == 39);
-
-      System.out.println(arrToString(arr));
+      int[][] arr2 = {{2,3,0},{5,6},{8,0,4,1,1}};
+      System.out.println(arr2DSum(arr2) == 30);
       
+      int[][] arr3 = {{2,3,0},{},{8,0,4,1,1}};
+      System.out.println(arr2DSum(arr3) == 19);
+
+      int[][] arr4 = {{2,3,4,3},{2,6,5,4},{8,0,4,1}};
+      System.out.println(arr2DSum(arr3) == 19);
+      
+      System.out.println(arrToString(arr));
+      System.out.println(arrToString(arr4));
+
+      System.out.print("array swap: are they same array? " + (swapRC(arr) == arr));
+      System.out.print(" First Array: " + arrToString(arr));
+      System.out.println(" Swapped Array: " + arrToString(swapRC(arr)));
+
+      System.out.print("array swap: are they same array? " + (swapRC(arr4) == arr4));
+      System.out.print(" First Array: " + arrToString(arr4));
+      System.out.println(" Swapped Array: " + arrToString(swapRC(arr4)));
 
       // int [] [] array{
       //{2, 3, 4},
@@ -75,7 +89,7 @@ public class ArrayMethods{
         
         int[][] newArray = new int[nums[0].length][nums.length];
         for (int i = 0; i < nums.length; i++){
-            for (int index = 0; i < nums[i].length; index++){
+            for (int index = 0; index < nums[i].length; index++){
                 newArray[index][i] = nums[i][index];
             }
         
