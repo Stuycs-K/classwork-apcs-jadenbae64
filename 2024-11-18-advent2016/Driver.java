9f929e8bd4ded9875a7ea0fae153b69bcd3ad92f
Jaden Bae <jbae50@stuy.edu>
2024-11-20 09:04:22 -0500

    returns total distance

--- 2024-11-18-advent2016/Driver.java
+++ 2024-11-18-advent2016/Driver.java
@@ -29,7 +29,7 @@ import java.util.Arrays;
      System.out.println(x);
      System.out.println(y);
 
-     return (x);
+     return (Math.abs(x) + Math.abs(y));
    }
    public static String [] parse(String file){
      try{
