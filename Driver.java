9d1843a2aa5c7546c0e0c86f3249b2e9d0beb0e6
Jaden Bae <jbae50@stuy.edu>
2024-12-05 09:08:01 -0500

    in process

--- Driver.java
+++ Driver.java
@@ -1,11 +1,33 @@
 public class Driver{
+    public static int rng(){
+        return((int)(Math.random() * 100));
+    }
     public static void main (String[]args){
         for (int i = 0; i < 30; i++){
             Text.color(Text.background(Text.YELLOW));
             if (i == 0 || i == 29){
                 System.out.print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
                 System.out.println(Text.RESET); 
-            
+            /*
+            }else if(i == 1){
+
+                int[] randomNumb = {rng(), rng(), rng()};
+
+                for (i = 0; i < 70; i++){
+                    if (randomNumb[i%3] < 25){
+                        Text.color(Text.background(Text.RED));
+                    }else if(randomNumb[i%3] > 75){
+                        Text.color(Text.background(Text.GREEN));
+                    }else{
+                        System.out.print(Text.RESET); 
+                    }
+
+                    System.out.print(randomNumb[i%3]);    
+
+                }
+
+                System.out.println(Text.RESET); 
+            */
             }else{
                System.out.print("b");
                System.out.print(Text.RESET);
