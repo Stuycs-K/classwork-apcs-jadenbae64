b04a2018ddd7cf9933d68d41b9236cd83934d425
Jaden Bae <jbae50@stuy.edu>
2024-12-05 08:58:19 -0500

    created borders

--- Driver.java
+++ Driver.java
@@ -3,14 +3,19 @@ public class Driver{
         for (int i = 0; i < 30; i++){
             Text.color(Text.background(Text.YELLOW));
             if (i == 0 || i == 29){
-                System.out.println("                                                                                ");
+                System.out.print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+                System.out.println(Text.RESET); 
+            
             }else{
-               System.out.println(" ");
+               System.out.print("b");
                System.out.print(Text.RESET);
-               System.out.print("                                                                              ");
+               System.out.print("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
                Text.color(Text.background(Text.YELLOW));
-               System.out.print(" "); 
+               System.out.print("b");
+               System.out.println(Text.RESET); 
+               
             }
+            
             
         }
         System.out.print(Text.RESET);
