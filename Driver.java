dcd5a5348380e53337aeaddd6e8b8be07f117108
Jaden Bae <jbae50@stuy.edu>
2024-12-05 08:45:09 -0500

    loop for backgroudn

---
+++ Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+    public static void main (String[]args){
+        Text.color(Text.background(Text.YELLOW));
+        for (int i = 0; i < 80; i++){
+            System.out.println("                              ");
+        }
+
+    }
+}
