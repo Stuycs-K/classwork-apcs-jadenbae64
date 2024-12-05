40eb968a1cff40da99441c013cdb03711ffb42c6
Jaden Bae <jbae50@stuy.edu>
2024-12-05 08:54:03 -0500

    added borders

--- Driver.java
+++ Driver.java
@@ -1,9 +1,19 @@
 public class Driver{
     public static void main (String[]args){
-        Text.color(Text.background(Text.YELLOW));
-        for (int i = 0; i < 80; i++){
-            System.out.println("                              ");
+        for (int i = 0; i < 30; i++){
+            Text.color(Text.background(Text.YELLOW));
+            if (i == 0 || i == 29){
+                System.out.println("                                                                                ");
+            }else{
+               System.out.println(" ");
+               System.out.print(Text.RESET);
+               System.out.print("                                                                              ");
+               Text.color(Text.background(Text.YELLOW));
+               System.out.print(" "); 
+            }
+            
         }
-
+        System.out.print(Text.RESET);
     }
+    
 }
