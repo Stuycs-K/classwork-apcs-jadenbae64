286aec716baaeae64a3ebe11e18d237293270266
Jaden Bae <jbae50@stuy.edu>
2024-12-10 07:55:36 -0500

    fixed bugs

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,24 +1,28 @@
+import java.util.Scanner;
+
 public class Game{
   public static void main(String[]args){
     CodeWarrior codeWarrior1 = new CodeWarrior();
-    Adventurer adventurer1 = new Adventurer();
+    Adventurer adventurer1 = new Warrior("Beowulf");
 
     System.out.println(codeWarrior1.getName()+", "+codeWarrior1.getHP()+", "+codeWarrior1.getSpecialName()+", "+codeWarrior1.getSpecial());
-    System.out.println(adventurer1.getName()+", "+adventurer1.getHP());
+    System.out.println(adventurer1.getName()+", "+adventurer1.getHP()+", "+adventurer1.getSpecialName()+", "+adventurer1.getSpecial());
+
+    Scanner userInput = new Scanner(System.in);
 
     while (true){
       System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
       String nextMove = userInput.nextLine();
 
-      if (nextMove.Equals("a")) {
+      if (nextMove.equals("a")) {
 
-      }else if (nextMove.Equals("sp")){
-
-
-      }else if (nextMove.Equals("su")){
+      }else if (nextMove.equals("sp")){
 
 
-      }else if (nextMove.Equals("quit")){
+      }else if (nextMove.equals("su")){
+
+
+      }else if (nextMove.equals("quit")){
         break;
 
       }else{
